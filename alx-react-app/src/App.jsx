168e7874b68430bcd2d7db494521bf384c585e55
favouriweala <favouriweala@gmail.com>
2025-10-29 16:13:30 +0100

    fix

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -14,9 +14,9 @@ function App() {
     
     <>
       <WelcomeMessage />
-      <Header/>
-      <MainContent/>
-      <Footer/>
+      <Header /> 
+      <MainContent />
+      <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
