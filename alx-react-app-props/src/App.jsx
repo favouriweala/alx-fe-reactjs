1330e018b4a8498bdc72a3adf4f176aed0dd50b5
favouriweala <favouriweala@gmail.com>
2025-11-09 21:40:53 +0100

    fix

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,20 +1,13 @@
-import { useState } from 'react';
+
+import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
-import UserProfile from './components/UserProfile';
 
 function App() {
-  const [userData] = useState({
-    name: "Alice",
-    age: 25,
-    bio: "Loves hiking and photography",
-  });
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
-      <div style={{ padding: "20px" }}>
-        <h1>Welcome to My Company</h1>
-        <UserProfile />
-      </div>
+      <ProfilePage />
     </UserContext.Provider>
   );
 }
