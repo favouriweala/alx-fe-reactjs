b534253b00cc63074c21f487ab2004c1792bda82
favouriweala <favouriweala@gmail.com>
2025-11-25 21:14:53 +0100

    update the project structure

--- github-user-search/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/Footer.jsx
-function Footer() {
-  return (
-    <footer className="bg-gray-200 text-center p-4 mt-10">
-      &copy; 2025 My GitHub Search App
-    </footer>
-  );
-}
-export default Footer;
