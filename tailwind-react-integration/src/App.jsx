d00bfccd34c84391ed5e927490a936b4c6cd2cba
favouriweala <favouriweala@gmail.com>
2025-11-27 17:33:11 +0100

    update rendering structure

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import'./components/UserProfile'
 import './App.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <UserProfile/>
   )
 }
 
--- tailwind-react-integration/src/main.jsx
+++ tailwind-react-integration/src/main.jsx
@@ -3,11 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './components/UserProfile.jsx'
-import UserProfile from './components/UserProfile.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <UserProfile/>
   </StrictMode>,
 )
